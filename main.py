4c1062fc7180aff334babeabeae5ff662ea670a6
Bessarab123 <bessarab.kostik@mail.ru>
2020-02-18 21:56:40 +0300

    Изменён способ ввода координат

--- main.py
+++ main.py
@@ -3,24 +3,30 @@ import sys
 import requests
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import QApplication, QDialog, QAbstractButton
+from PyQt5.QtWidgets import QApplication, QDialog, QAbstractButton, QInputDialog
 from Widget import Ui_Dialog
 
 
 class MyMap(QDialog, Ui_Dialog):
-    def __init__(self, bts):
+    def __init__(self):
+        while True:
+            coor, boo = QInputDialog.getText(None, "Введите координаты", "Формат 37.575636,54.171069")
+            if boo:
+                try:
+                    list(map(float, coor.split(',')))
+                    map_params["ll"] = coor
+                    break
+                except Exception:
+                    pass
+            else:
+                sys.exit()
         super().__init__()
         self.setupUi(self)
 
         self.pushButton.clicked.connect(self.search)
         self.pushButton_2.clicked.connect(self.discharge)
-
-        p = QPixmap()
-        buf = bts.getbuffer()
-        p.loadFromData(buf)
-        self.label.setPixmap(p)
-
         self.buttonGroup.buttonClicked.connect(self.change_map_sloi)
+        self.update_im()
 
     def keyPressEvent(self, event):
         if event.key() == Qt.Key_PageUp:
@@ -79,23 +85,17 @@ class MyMap(QDialog, Ui_Dialog):
         self.label.setPixmap(p)
 
 
-address_ll = list(map(float, input('формат вводных данных: 37.575636,54.171069\n').split(',')))
-size = '650,450'
 map_params = {
+    "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
     "l": 'map',
-    "ll": ','.join(map(str, address_ll)),
-    'size': size,
+    "ll": None,
+    'size': '650,450',
     'z': 15}
 
 map_api_server = "http://static-maps.yandex.ru/1.x/"
-response = requests.get(map_api_server, params=map_params)
-if not response:
-    print("Ошибка выполнения запроса:")
-    print("Http статус:", response.status_code, "(", response.reason, ")")
-    sys.exit(1)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    maps = MyMap(io.BytesIO(response.content))
+    maps = MyMap()
     maps.show()
     sys.exit(app.exec_())
