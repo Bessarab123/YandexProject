4234449aa57964608e7e54e4dd8dfe0a5badd53e
Bessarab123 <bessarab.kostik@mail.ru>
2020-02-18 22:28:17 +0300

    Добавлено перемещение

--- main.py
+++ main.py
@@ -36,16 +36,16 @@ class MyMap(QDialog, Ui_Dialog):
             self.change_mashtab('Down')
 
         if event.key() == Qt.Key_Up:
-            self.change_centre_coords()
+            self.change_centre_coords('Up')
 
         if event.key() == Qt.Key_Down:
-            self.change_centre_coords()
+            self.change_centre_coords('Down')
 
         if event.key() == Qt.Key_Left:
-            self.change_centre_coords()
+            self.change_centre_coords('Left')
 
         if event.key() == Qt.Key_Right:
-            self.change_centre_coords()
+            self.change_centre_coords('Right')
 
     def change_mashtab(self, s):
         if s == 'Up':
@@ -54,8 +54,18 @@ class MyMap(QDialog, Ui_Dialog):
             map_params['z'] -= 1 if map_params['z'] != 0 else 0
         self.update_im()
 
-    def change_centre_coords(self):
-        pass
+    def change_centre_coords(self, s):
+        coor = list(map(float, map_params['ll'].split(',')))
+        if s == "Up":
+            coor[1] += (20 - map_params['z']) * 0.005
+        elif s == "Down":
+            coor[1] -= (20 - map_params['z']) * 0.005
+        elif s == "Left":
+            coor[0] -= (20 - map_params['z']) * 0.005
+        elif s == "Right":
+            coor[0] += (20 - map_params['z']) * 0.005
+        map_params['ll'] = ','.join(list(map(str, coor)))
+        self.update_im()
 
     def change_map_sloi(self):
         text = self.buttonGroup.checkedButton().text()
@@ -76,9 +86,10 @@ class MyMap(QDialog, Ui_Dialog):
     def update_im(self):
         response = requests.get(map_api_server, params=map_params)
         if not response:
+            print(response.url)
             print("Ошибка выполнения запроса:")
             print("Http статус:", response.status_code, "(", response.reason, ")")
-            sys.exit(2)
+            sys.exit(1)
         p = QPixmap()
         buf = (io.BytesIO(response.content)).getbuffer()
         p.loadFromData(buf)
